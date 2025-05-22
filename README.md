1f1ee1bcebe9a6720f1b2f29c4e74cbd2b9fbe0e
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-05-22 16:44:43 +0000

    thank u for workinggg

---
+++ README.md
@@ -0,0 +1,18 @@
+[![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-2972f46106e565e64193e422d61a12cf1da4916b45550586e14ef0a7c637dd04.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=19311355)
+# ICS2O ⇢ Unit #5-03
+
+[![Mr Coxall's Super Linter](https://github.com/<OWNER>/<REPOSITORY>/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/<OWNER>/<REPOSITORY>/actions)
+
+[![Deploy Pages](https://github.com/<OWNER>/<REPOSITORY>/workflows/Deploy%20Pages/badge.svg)](https://github.com/<OWNER>/<REPOSITORY>/actions)
+
+This site can be found at: [https://<OWNER>.github.io/<REPOSITORY>](https://<OWNER>.github.io/<REPOSITORY>)
+
+---
+
+**NOTES ON INITIAL LOAD:**
+- remember to goto on the menu bar:
+  - File
+  - Go to Repository
+  - ⚙ Settings
+  - 🗔 Pages
+    - then from the "Deploy from a branch ▼" button, select "GitHub Actions"
---
+++ js/script.js
@@ -0,0 +1,46 @@
+// Copyright (c) 2025 Shanea Jaromay All rights reserved
+//
+// Created by: Shanea Jaromay
+// Created on: Apr 2025
+// This file contains the JS functions for index.html
+
+/**
+ * This function finds the type of movie for your age
+ */
+// eslint-disable-next-line no-unused-vars
+
+function findMovieType() {
+  // input
+  const ageNumber = document.getElementById('age-number').value
+
+  //Save input values to localStorage
+  localStorage.setItem('ageNumber', ageNumber)
+
+  // process
+  if (ageNumber >= 17) {
+    // output
+    document.getElementById('result').innerHTML = 
+    'You can see a R movie alone.';
+  } else if (ageNumber >= 13) {
+    // output
+    document.getElementById('result').innerHTML = 
+    'You can see a PG-13 movie alone.';
+  } else if (ageNumber >= 5) {
+    // output
+    document.getElementById('result').innerHTML = 
+    'You can see a G or PG movie alone.';
+  } else {
+    // output
+    document.getElementById('result').innerHTML = 
+    'Kiddo, you are too young for most of the movies.'
+  }
+}
+
+// This function loads previous values from localStorage
+function loadSavedValues() {
+  const savedAgeNumber = localStorage.getItem('ageNumber')
+  if (savedAgeNumber) document.getElementById('age-number').value = savedAgeNumber
+}
+
+// Run this when the page is loaded
+window.onload = loadSavedValues
